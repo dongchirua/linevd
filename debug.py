3ed5969555cd9abe7b437f5bfecedba09c1514db
Bryan Nguyen <dongchirua@live.com>
2023-01-07 02:41:17 +1100

    test

---
+++ debug.py
@@ -0,0 +1,5 @@
+import sastvd.helpers.joern as svdj
+import sastvd.ivdetect.helpers as svdh
+
+svdh.feature_extraction('storage/processed/bigvul/before/88626.c')
+# svdh.feature_extraction('storage/processed/bigvul/before/53.c')  # ERROR
